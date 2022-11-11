f367dfaef10acbfe39ddad6a590cfdea691121d3
zelfroster <sumitkumarsoni123@gmail.com>
2022-11-11 23:47:07 +0530

    Added Navbar Component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   theme: {
     extend: {},
+    colors: {
+      backgroundColor: "#E6E0EC",
+      primaryColor: "#36018C",
+      secondaryColor: "#FD0299",
+      gradientPink: "#F82CA6",
+      gradientPurple: "#410B98",
+      playlistBg: "#C0B1E0",
+    },
   },
   plugins: [],
 };
