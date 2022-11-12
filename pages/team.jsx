c8836fb7ccbb1a46b943f87cfb4d1d08440c451c
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-11-12 12:12:50 +0000

    create our team page

--- pages/team.jsx
+++ pages/team.jsx
@@ -8,7 +8,6 @@ function team() {
       <main className='w-screen min-h-[90vh] flex flex-col align-center'>
         <h1 className='text-3xl font-bold text-center p-6'>Meet our team</h1>
         <section className='w-full p-10'>
-
         <div className='grid place-items-center w-full h-full bg-red-500 p-8'>
             <div className='w-[300px] h-[150px] bg-red-400'>
               <Image src={follow.svg} alt='something'/>
