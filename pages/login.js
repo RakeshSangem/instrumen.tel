ded98fbc06bcab5b86726d4c99f10e3069943c67
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2022-11-13 03:38:59 +0530

    Spotify nextAuth

---
+++ pages/login.js
@@ -0,0 +1,28 @@
+import {getProviders, signIn} from "next-auth/react";
+
+function Login({providers}){
+    return (
+        <div className="flex flex-col items-center min-h-screen w-full justify-center">
+            {Object.values(providers).map((provider) => (
+                <div key = {provider.name}>
+                    <button className="bg-[#18D860] text-white p-5 rounded-full" 
+                    onClick={() => signIn(provider.id, 
+                        {callbackUrl:"/playlist"})}>
+                        Login using {provider.name}</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Login;
+
+export  async function getServerSideProps(){
+    const providers = await getProviders();
+
+    return{
+        props:{
+            providers,
+        },
+    };
+}
