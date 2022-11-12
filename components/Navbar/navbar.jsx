687c40a42b2bb517cf57ae75983bf733f3e583cc
zelfroster <sumitkumarsoni123@gmail.com>
2022-11-12 13:36:52 +0530

    created HomePage and added links to Navbar

--- components/Navbar/navbar.jsx
+++ components/Navbar/navbar.jsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 import projectLogo from "../../public/project-logo.svg";
 import githubLogo from "../../public/githubLogo.svg";
 
 function Navbar() {
   return (
-    <nav className="w-full h-[10vh] flex items-center justify-between px-16 bg-backgroundColor">
-      <Image src={projectLogo} alt="project-logo" className="w-44"></Image>
-      <Image src={githubLogo} alt="project-logo"></Image>
+    <nav className="w-full h-[10vh] flex items-center justify-between px-16 bg-backgroundColor z-50">
+      <Link href="/">
+        <Image src={projectLogo} alt="project-logo" className="w-44"></Image>
+      </Link>
+      <Link href="https://github.com/RakeshSangem/instrumen.tel">
+        <Image src={githubLogo} alt="project-logo"></Image>
+      </Link>
     </nav>
   );
 }
