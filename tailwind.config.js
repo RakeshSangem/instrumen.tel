4965e495fa55f36f670ecf7fdf5583538550e06c
Sumit Kumar Soni <93514460+zelfroster@users.noreply.github.com>
2022-11-12 17:18:13 +0530

    feat: created Homepage and added links to Navbar (#16)
    * created HomePage and added links to Navbar
    
    * Updated Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 module.exports = {
   content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   theme: {
-    extend: {},
-    // colors: {
-    //   'backgroundColor': "#E6E0EC",
-    //   'primaryColor': "#36018C",
-    //   'secondaryColor': "#FD0299",
-    //   'gradientPink': "#F82CA6",
-    //   'gradientPurple': "#410B98",
-    //   'playlistBg': "#C0B1E0",
-    //   'playlistsong': "#C4B9DB",
-    // },
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+      },
+    },
+    colors: {
+      backgroundColor: "#E6E0EC",
+      primaryColor: "#36018C",
+      secondaryColor: "#FD0299",
+      gradientPink: "#F82CA6",
+      gradientPurple: "#410B98",
+      playlistBg: "#C0B1E0",
+      playlistsong: "#C4B9DB",
+    },
   },
   plugins: [],
 };
