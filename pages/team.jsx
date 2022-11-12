16efa7a9d05453b4787398a36ba83045fc580ac3
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-11-12 12:12:50 +0000

    create our team page

---
+++ pages/team.jsx
@@ -0,0 +1,24 @@
+
+import Navbar from '../components/Navbar/navbar'
+import Image from "Next/Image"
+
+function team() {
+  return (
+    <section>
+      <Navbar />
+      <main className='w-screen min-h-[90vh] flex flex-col align-center'>
+        <h1 className='text-3xl font-bold text-center p-6'>Meet our team</h1>
+        <section className='w-full p-10'>
+
+        <div className='grid place-items-center w-full h-full bg-red-500 p-8'>
+            <div className='w-[300px] h-[150px] bg-red-400'>
+              <Image src={follow.svg} alt='something'/>
+            </div>
+        </div>
+        </section>
+      </main>
+    </section>
+  )
+}
+
+export default team
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,15 @@ module.exports = {
   content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   theme: {
     extend: {},
-    colors: {
-      backgroundColor: "#E6E0EC",
-      primaryColor: "#36018C",
-      secondaryColor: "#FD0299",
-      gradientPink: "#F82CA6",
-      gradientPurple: "#410B98",
-      playlistBg: "#C0B1E0",
-      playlistsong: "#C4B9DB",
-    },
+    // colors: {
+    //   'backgroundColor': "#E6E0EC",
+    //   'primaryColor': "#36018C",
+    //   'secondaryColor': "#FD0299",
+    //   'gradientPink': "#F82CA6",
+    //   'gradientPurple': "#410B98",
+    //   'playlistBg': "#C0B1E0",
+    //   'playlistsong': "#C4B9DB",
+    // },
   },
   plugins: [],
 };
