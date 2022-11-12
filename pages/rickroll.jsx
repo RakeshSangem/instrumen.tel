464d65b04037e7894532a0be0b128ec840b85717
Mayank <mayank.ks.2002@gmail.com>
2022-11-12 14:45:37 +0530

    Update rickroll.jsx

--- pages/rickroll.jsx
+++ pages/rickroll.jsx
@@ -12,9 +12,9 @@ export default function RickRollPage() {
                 <div className="flex items-center px-40 p-10 py-10">
                     <div className="p-5 text-lg">
                     It is a prank and internet meme involving an unexpected appearance of a music video 
-                    "Never Gonna Give You Up" by Rick Astley which hit number 1 in top 100 in March of 1988. 
+                    &quot;Never Gonna Give You Up&quot; by Rick Astley which hit number 1 in top 100 in March of 1988. 
                     It also has a long journey on how it became a meme, it started when it gained popularity on the internet
-                    after it's appearance in "It's always sunny in philadelphia" 
+                    after it&apos;s appearance in &quot;It&apos;s always sunny in philadelphia&quot;
                     </div>
 
                     <Image
@@ -36,7 +36,7 @@ export default function RickRollPage() {
                     Then the creator of 4chan created a word filter which changed the word from egg &#8594; duck
                     which started a new meme when it changed the word eggroll to duckroll and this img popped up
                     people started generating links which would misdirect the people to this image and when GTA 4 
-                    came out and people started linking to it's trailer but it took them to this video and thus Rickrolling was
+                    came out and people started linking to it&apos;s trailer but it took them to this video and thus Rickrolling was
                     born.
                     </div>
                     
