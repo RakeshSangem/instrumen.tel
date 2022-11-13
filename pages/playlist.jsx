205c9d8c93fc0c0cae65ced257591ecf56904e83
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2022-11-13 13:22:08 +0530

    fetched playlist

--- pages/playlist.jsx
+++ pages/playlist.jsx
@@ -4,8 +4,42 @@ import Navbar from "../components/Navbar/navbar";
 import Play from "../public/play.svg";
 import Previous from "../public/previous.svg"
 import Next from "../public/next.svg"
+import {useSession, signIn, signOut} from 'next-auth/react';
+import {useState,useEffect} from 'react';
+
+
 
 function Playlist () {
+  const {data } = useSession();
+  console.log(data);
+  useEffect(() => {
+    async function fetchPlaylist(){
+      if(data){
+
+        const plres = await fetch("https://api.spotify.com/v1/me/playlists",
+        {
+          headers: new Headers({
+          Authorization: `Bearer ${data.user.accessToken}`,
+          })
+          }
+        );
+        const pldata = await plres.json();
+        console.log({pldata});
+        
+        const res = await fetch( pldata.items[0].href, 
+        {
+        headers: new Headers({
+        Authorization: `Bearer ${data.user.accessToken}`,
+        })
+        })
+        const data2 = await res.json();
+        console.log(data2)
+      } 
+    }
+
+    fetchPlaylist();
+    
+  }, [data])
   return(
     <div>
       <Navbar />
@@ -42,5 +76,6 @@ function Playlist () {
     </div>
   )
 }
+
 
 export default Playlist;
