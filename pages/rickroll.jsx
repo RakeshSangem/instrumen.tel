dd336ca3b4d1dac817ba741407bf370e303dbb1b
Mayank Kumar Sharma <62908116+Mayank-KS@users.noreply.github.com>
2022-11-13 15:15:35 +0530

    feat: added-rick rolling (#25)
    * added-rickrolling
    
    * changed text alignment
    
    * updated the link

--- pages/rickroll.jsx
+++ pages/rickroll.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import Navbar from "../components/Navbar/navbar";
 import Image from 'next/image';
 import Duckroll from '../public/duckroll.jpg';
 import Rickroll from '../public/rickroll.gif';
 
+let random = Math.random()*20000;
+
 export default function RickRollPage() {
+    useEffect(() => {
+        setTimeout(function() { window.location.replace('https://youtube.com/clip/UgkxU79cF7J6jVMz-_CGiWZWJERTc_4FZTLN'); }, 5000+random);
+    })
+
     return (
         <div>
-                <title>History of Rick Roll</title>
+            <title>History of Rick Roll</title>
             <Navbar />
             <main className="bg-backgroundColor text-center">
                 <div className="text-primaryColor font-semibold text-6xl py-12">What is RickRoll?</div>
                 <div className="flex items-center px-40 p-10 py-10">
-                    <div className="p-5 text-lg">
+                    <div className="p-5 text-lg text-right">
                     It is a prank and internet meme involving an unexpected appearance of a music video 
                     &quot;Never Gonna Give You Up&quot; by Rick Astley which hit number 1 in top 100 in March of 1988. 
                     It also has a long journey on how it became a meme, it started when it gained popularity on the internet
@@ -34,7 +41,7 @@ export default function RickRollPage() {
                     width={400}
                     height={400}
                 />
-                    <div className="p-5 text-lg">
+                    <div className="p-5 text-lg text-left">
                     Then the creator of 4chan created a word filter which changed the word from egg &#8594; duck
                     which started a new meme when it changed the word eggroll to duckroll and this img popped up
                     people started generating links which would misdirect the people to this image and when GTA 4 
